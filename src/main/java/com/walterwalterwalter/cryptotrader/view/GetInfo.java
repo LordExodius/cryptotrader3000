989ae3bdb79ef9cc6cf09a3ccecea29f53dcb3a3
basokant <basokanthan@gmail.com>
2022-03-28 21:44:20 -0400

    create classes

---
+++ src/main/java/com/walterwalterwalter/cryptotrader/view/GetInfo.java
@@ -0,0 +1,10 @@
+package com.walterwalterwalter.cryptotrader.view;
+
+public @interface GetInfo {
+    public TradingBroker getBroker();
+    public TradingStrategy getStrategy();
+    public String getCoinName();
+    public String getActionType();
+    public int getQuantity();
+    public double getPrice();
+}
---
+++ src/main/java/com/walterwalterwalter/cryptotrader/view/TradeActivityTable.java
@@ -0,0 +1,5 @@
+package com.walterwalterwalter.cryptotrader.view;
+
+public class TradeActivityTable {
+    
+}
