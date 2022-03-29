d931d5d43b83e336a6afa754fbcfdba08fa14678
LordExodius <oscar45697@gmail.com>
2022-03-28 21:35:54 -0400

    added frame for LoginServer

---
+++ src/main/java/com/walterwalterwalter/cryptotrader/authentication/LoginServer.java
@@ -0,0 +1,26 @@
+package com.walterwalterwalter.cryptotrader.authentication;
+import java.sql.*;
+
+public class LoginServer {
+    // Select database file for user info
+    Connection connection;
+
+    public LoginServer()
+    {
+        try{
+            // change path to working one
+            connection = DriverManager.getConnection("jdbc:sqlite:/db/auth.db");
+        }
+            
+        catch(SQLException e) {
+            System.out.println("An SQLException has occurred. Error message:");
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        LoginServer test = new LoginServer();
+    }
+    
+}
