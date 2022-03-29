1e739ab093d7ec4e853e23a77a0d5859c4aee765
Oscar Yu <oscar45697@gmail.com>
2022-03-29 13:36:44 -0400

    Added SHA-256 hashing to passwords for storage and authentication; created helper methods (temporary?) to add/delete/display database entries

--- src/main/java/cryptotrader/authentication/Database.java
+++ src/main/java/cryptotrader/authentication/Database.java
@@ -1,6 +1,10 @@
 package cryptotrader.authentication;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.Base64;
 
 public class Database implements DatabaseAuthenticate {
     // NOTE: Implement GetFromDatabase when TraderList and TradeLog are aded
@@ -44,18 +48,31 @@ public class Database implements DatabaseAuthenticate {
     @Override
     public boolean authenticate(String username, String password) 
     {
+        // Count rows with matching user/hashed password combination
         String sql = "SELECT COUNT(*) as total FROM creds WHERE user = ? AND pass = ?";
         try 
         {
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, username);
-            statement.setString(2, password);
+
+            // hash password
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            String passEncoded = Base64.getEncoder().encodeToString(hash);
+            statement.setString(2, passEncoded);
+
             ResultSet result = statement.executeQuery();
             int matches = result.getInt("total");
             result.close();
 
+            // if matching row exists, return true
             if(matches > 0)
                 return true;
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            System.out.println("Error hashing password:");
+            System.out.println(e);
         }
         catch(SQLException e) 
         {
@@ -70,5 +87,98 @@ public class Database implements DatabaseAuthenticate {
         if(instance == null)
             instance = new Database();
         return instance;
+    }
+
+    // ---------------------------------------------------------------------------
+    // Database helper functions
+
+    // add new row entry to database table creds with given username and hashed password
+    public boolean addUser(String username, String password)
+    {
+        String check = "SELECT COUNT(*) as total FROM creds WHERE user = ?";
+        try
+        {
+            PreparedStatement statement = connection.prepareStatement(check);
+            statement.setString(1, username);
+            ResultSet result = statement.executeQuery();
+            int matches = result.getInt("total");
+            result.close();
+
+            if(matches == 0)
+            {
+                String add = "INSERT INTO creds(user, pass) VALUES(?, ?)";
+                statement = connection.prepareStatement(add);
+                statement.setString(1, username);
+
+                // hash password
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+                String passEncoded = Base64.getEncoder().encodeToString(hash);
+                statement.setString(2, passEncoded);
+
+                statement.executeUpdate();
+                return true;
+            }
+            else
+            {
+                System.out.println("User already exists. Not added.");
+            }
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            System.out.println("Error hashing password:");
+            System.out.println(e);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("An SQL error has occured during authentication:");
+            System.out.println(e);
+        }
+        return false;
+    }
+
+    // delete all entries in database table creds
+    public void clearCreds()
+    {
+        String clear = "DELETE FROM creds";
+        try
+        {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(clear);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("An SQL error has occured during authentication:");
+            System.out.println(e);
+        }
+    }
+
+    // display table containing all username/hashed password pairs
+    public void showCreds()
+    {
+        String selectAll = "SELECT * FROM creds";
+        try
+        {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(selectAll);
+            while(result.next())
+            {
+                String user = result.getString("user");
+                String pass = result.getString("pass");
+                System.out.println("User: " + user + " | Pass: " + pass);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("An SQL error has occured during authentication:");
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        //Database.getInstance().addUser("admin", "password");
+        // Database.getInstance().clearCreds();
+        Database.getInstance().showCreds();
     }
 }
