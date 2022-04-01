fd8870562ba19c20a50c8b396162bba10a7b51fa
basokant <basokanthan@gmail.com>
2022-04-01 19:03:56 -0400

    import TradingBroker and TradingStrategy

--- src/main/java/cryptotrader/view/GetTradeInfo.java
+++ src/main/java/cryptotrader/view/GetTradeInfo.java
@@ -1,8 +1,11 @@
 package cryptotrader.view;
 
+import cryptotrader.trade.TradingBroker;
+import cryptotrader.trade.TradingStrategy;
+
 public interface GetTradeInfo {
-    // public TradingBroker getBroker();
-    // public TradingStrategy getStrategy();
+    public TradingBroker getBroker();
+    public TradingStrategy getStrategy();
     public String getCoinName();
     public String getActionType();
     public int getQuantity();
