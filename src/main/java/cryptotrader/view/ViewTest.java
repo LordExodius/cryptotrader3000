ec7f2a0fbea50fb746d82b06faade7e9cbd1819e
basokant <basokanthan@gmail.com>
2022-04-02 16:37:11 -0400

    change ViewTest

--- src/main/java/cryptotrader/view/ViewTest.java
+++ src/main/java/cryptotrader/view/ViewTest.java
@@ -1,9 +1,26 @@
 package cryptotrader.view;
 
+import cryptotrader.trade.TradingBroker;
+
 public class ViewTest {
 
     public static void main(String[] args) {
         
+        TradingBroker broker1 = new TradingBroker();
+        TradingBroker broker2 = new TradingBroker();
+        TradingBroker broker3 = new TradingBroker();
+        TradingBroker broker4 = new TradingBroker();
+
+        StrategyB strategyB = new StrategyB();
+        StrategyC strategyC = new StrategyC();
+        StrategyD strategyD = new StrategyD();
+
+        // TEST 1: Adding trade results to TradeLog
+
+        
+
+        
+
     }
     
 }
