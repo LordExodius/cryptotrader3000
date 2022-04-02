58e4b48324e1f22bcfd664d905cba7f75e895b5f
Jackson Howe <jacksonhowe3@gmail.com>
2022-04-01 20:17:15 -0400

    add logic for tradingStrategy

--- src/main/java/cryptotrader/trade/TradingStrategy.java
+++ src/main/java/cryptotrader/trade/TradingStrategy.java
@@ -2,21 +2,35 @@ package cryptotrader.trade;
 
 import cryptotrader.view.TradeResult;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 
 import com.google.gson.JsonObject;
 
 public abstract class TradingStrategy implements Trade {
 
-    private ArrayList<String> coins;
+    private HashMap<String, Coin> coins;
 
     @Override
-    public TradeResult trade(JsonObject coinInfo) {
+    public TradeResult trade(JsonObject coinData) {
         // TODO Auto-generated 
         return null;
     }
 
-    public ArrayList<String> getCoins() {
-        return this.coins;
+    /**
+     * 
+     * @param coinsIn
+     * @return boolean
+     */
+    public boolean checkCoins(HashMap<String, Coin> coinsIn) {
+        Set interestedCoins = coinsIn.keySet();
+        for (String coinTitle : coins.keySet()) {
+            if (!(interestedCoins.contains(coinTitle))) {
+                return false;
+            }
+        }
+        return true;
     }
     
 }
