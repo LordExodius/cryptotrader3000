c8ba6098661cb2328e2a30c6b54bf1dfb5bdc867
Jackson Howe <jacksonhowe3@gmail.com>
2022-04-01 20:54:08 -0400

    Code TradingStrategy methods

--- src/main/java/cryptotrader/trade/TradingStrategy.java
+++ src/main/java/cryptotrader/trade/TradingStrategy.java
@@ -3,29 +3,33 @@ package cryptotrader.trade;
 import cryptotrader.view.TradeResult;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.HashSet;
 
-
-import com.google.gson.JsonObject;
 
 public abstract class TradingStrategy implements Trade {
 
-    private HashMap<String, Coin> coins;
-
-    @Override
-    public TradeResult trade(JsonObject coinData) {
-        // TODO Auto-generated 
-        return null;
-    }
+    private ArrayList<String> coins;
 
     /**
-     * 
+     * Constructor
+     * @param coins
+     */
+    protected TradingStrategy(ArrayList<String> coins) {
+        this.coins = coins; 
+    }
+
+    @Override
+    abstract public TradeResult trade(HashMap<String, Coin> interestedCoins);
+
+    /**
+     * Checks to see if the list of the coins involved in the trade contains all coins
+     * TradingStrategy requires to do the trade.
      * @param coinsIn
      * @return boolean
      */
-    public boolean checkCoins(HashMap<String, Coin> coinsIn) {
-        Set interestedCoins = coinsIn.keySet();
-        for (String coinTitle : coins.keySet()) {
+    protected boolean checkCoins(ArrayList<String> coinsIn) {
+        HashSet<String> interestedCoins = new HashSet<String>(coinsIn);
+        for (String coinTitle : coins) {
             if (!(interestedCoins.contains(coinTitle))) {
                 return false;
             }
