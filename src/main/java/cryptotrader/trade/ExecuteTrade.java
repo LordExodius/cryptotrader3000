2369f2c9de5e31cc9273787fb13870942824568c
David Tran <davidtranhq@gmail.com>
2022-04-03 21:54:24 -0400

    Update executeTrade() interface with HashMap

--- src/main/java/cryptotrader/trade/ExecuteTrade.java
+++ src/main/java/cryptotrader/trade/ExecuteTrade.java
@@ -1,7 +1,8 @@
 package cryptotrader.trade;
 
+import java.util.HashMap;
 import cryptotrader.view.TradeResult;
 
 public interface ExecuteTrade {
-    public TradeResult executeTrade();
+    public TradeResult executeTrade(HashMap<String, Coin> coinInfo);
 }
