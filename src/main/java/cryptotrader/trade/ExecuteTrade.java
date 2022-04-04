411d76db71e4b01c0820ed61f118500d0981aa20
David Tran <davidtranhq@gmail.com>
2022-04-03 22:05:31 -0400

    Rename coinInfo -> interestedCoins

--- src/main/java/cryptotrader/trade/ExecuteTrade.java
+++ src/main/java/cryptotrader/trade/ExecuteTrade.java
@@ -5,9 +5,5 @@ import cryptotrader.view.TradeResult;
 import java.util.HashMap;
 
 public interface ExecuteTrade {
-<<<<<<< HEAD
-    public TradeResult executeTrade(HashMap<String, Coin> interestedCoins);
-=======
     public TradeResult executeTrade(HashMap<String, Coin> coinInfo);
->>>>>>> 8947127275bb33703e96dd6cdcdd4e83e4253c0a
 }
